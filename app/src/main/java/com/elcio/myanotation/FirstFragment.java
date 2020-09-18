df20ca1802adbb02dac79360f254d093735b0110
Elcio Cestari Taira <elciotaira@gmail.com>
2020-09-18 16:15:42 -0400

    In this commit I implemented the handling with editText, where the user input a message and this message I catch (but stil not save) and show a postive our negative message depending on the user message.

--- app/src/main/java/com/elcio/myanotation/FirstFragment.java
+++ app/src/main/java/com/elcio/myanotation/FirstFragment.java
@@ -4,20 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class FirstFragment extends Fragment {
+    private View view;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/elcio/myanotation/MainActivity.java
+++ app/src/main/java/com/elcio/myanotation/MainActivity.java
@@ -1,5 +1,7 @@
 package com.elcio.myanotation;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,22 +13,51 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
+/**
+ * @author elcio cestari taira
+ * @version 0.1
+ *
+ * This Class is responsible to deal with all UI
+ *
+ */
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
+    private String userMessage;
+    private EditText editMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editMessage = findViewById(R.id.editMessage);
+
+        //Set preferences to be save on user device
+        preferences = getSharedPreferences(getString(R.string.shared_preferences_message), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                String showMessageAlert = "";
+                userMessage = editMessage.getText().toString();
+
+                if (userMessage.equals("")) {
+                    showMessageAlert = getString(R.string.wrong_input_message);
+                } else {//main area of the code, in this peace of code is deal with load user message in the device.
+                    showMessageAlert = getString(R.string.positve_input_message);
+                    //TODO
+                    //I need to implement this funcionality (save message with shared preferences)
+
+                }
+                Snackbar.make(view, showMessageAlert, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
     }//end's onCreate method
 
 }//end's MainActivity class
